05d27bd5d8a222c031abab600c9707fdeee18414
Elisabeth Manalo <eliman24@bergen.org>
2023-10-19 11:01:44 -0400

    Initial Commit

---
+++ Kotton App/Kotton_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kotton_AppApp.swift
+//  Kotton App
+//
+//  Created by Elisabeth Manalo on 10/19/23.
+//
+
+import SwiftUI
+
+@main
+struct Kotton_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
